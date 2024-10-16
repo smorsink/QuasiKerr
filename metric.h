40a678fa479c1465acdd46acac11c0b35573378a
Sharon Morsink <morsink@ualberta.ca>
2024-10-16 10:16:11 -0600

    Header Files

---
+++ metric.h
@@ -0,0 +1,5 @@
+
+void metric(double r, double theta, double gmunu[5], double a_bh, double epsilon);
+
+
+void gammas(double r, double theta, double Gamma[4][4][4], double a_bh, double epsilon);
---
+++ quadrupole.h
@@ -0,0 +1,5 @@
+double quadrupole(EOS *eos, NeutronStar *star);
+
+int BLSetUp(NeutronStar *star);
+
+int BLComputeMetric(NeutronStar *star, double rbl, double theta);
